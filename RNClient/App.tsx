154c1c56dc52147c7dcba673585e4c4f9b25d4c9
Dainius Trijonis <dainius.trijonis@gmail.com>
2020-11-27 04:54:49 +0200

    Bottom nav

--- RNClient/App.tsx
+++ RNClient/App.tsx
@@ -1,28 +1,55 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-function App() {
+import HomeScreen from './src/screens/Home'
+import WishListScreen from './src/screens/WishList'
+import ProfileScreen from './src/screens/Profile'
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen 
+              name="Home" 
+              component={HomeScreen}
+              options= {{ 
+                title: 'Home',
+                tabBarIcon:({color:tintColor})=>(  
+                  <Icon name="home" color={tintColor} size={25}/>  
+                )
+              }}
+          />
+          <Tab.Screen 
+              name="Login" 
+              component={WishListScreen} 
+              options= {{ 
+                  title: 'Wish List', 
+                  tabBarIcon:({color:tintColor})=>(  
+                    <Icon name="list-alt" color={tintColor} size={25}/>  
+                  )  
+              }}
+
+          />
+          <Tab.Screen 
+              name="Register" 
+              component={ProfileScreen} 
+              options= {{ 
+                title: 'Profile',
+                tabBarIcon:({color:tintColor})=>(  
+                  <Icon name="user" color={tintColor} size={25}/>  
+                )
+              }}  
+          />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
---
+++ RNClient/src/screens/WishList.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
+
+interface Props {
+    navigation: any;
+  }
+  export type AppState = {
+    user: any;
+    initializing: boolean;
+  };
+
+export default class WishList extends React.Component<Props> {
+  state: AppState = {
+      user: null,
+      initializing: true,
+  };
+  componentDidMount = () => {
+  };
+
+  render() {
+    if (this.state.initializing) {
+    return (
+        <SafeAreaView style={{backgroundColor: 'black'}}>
+        <View />
+        </SafeAreaView>
+    );
+    }
+  }
+  }
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+  layout: {
+    flex: 2,
+    alignContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: 300,
+    marginTop: 16,
+  },
+});
